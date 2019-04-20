2aec858a4cc5a8affded370ffa1e6af8575d5f1f
Michael Nolan <mtnolan2640@gmail.com>
2019-04-20 18:00:51 -0400

    Partially working fir filter

--- src/main/scala/singlesdr/IQDecimator.scala
+++ src/main/scala/singlesdr/IQDecimator.scala
@@ -2,6 +2,7 @@ package singlesdr
 
 import spinal.core._
 import spinal.lib._
+import scala.math._
 
 case class Complex(width:BitCount) extends Bundle {
   val i = SInt(width)
@@ -11,25 +12,45 @@ case class Complex(width:BitCount) extends Bundle {
 class IQDecimator extends Component {
   val widthin = 10 bits
   val widthout = 18 bits
-  val coeffs = Array(
-    -0.00566096,
-    0.13265074,
-    0.27096242,
-    0.34009388,
-    0.27096242,
-    0.13265074,
-    -0.00566096
+  val coeffs = Array[Float](
+    -0.00566096f,
+    0.13265074f,
+    0.27096242f,
+    0.34009388f,
+    0.27096242f,
+    0.13265074f,
+    -0.00566096f
   )
   val io = new Bundle {
     val cpx_in = in(Complex(widthin))
     val cpx_out = out(Complex(widthout))
   }
 
-  val delayline = Reg(Vec(Complex(widthout), coeffs.length))
+  val delayline = Reg(Vec(Complex(widthin), coeffs.length)) 
   for (i <- 1 to coeffs.length - 1) {
+    delayline(i).i init(0)
+    delayline(i).q init(0)
     delayline(i) := delayline(i-1)
   }
   delayline(0).i := io.cpx_in.i.resized
   delayline(0).q := io.cpx_in.q.resized
-  io.cpx_out := delayline(coeffs.length - 1)
+  io.cpx_out.q := delayline(coeffs.length - 1).q.resized
+
+  val mult_out = Vec(SInt(widthout), coeffs.length/2 + 1)
+  val sum = Vec(SInt(widthin + (1 bits)), coeffs.length/2 + 1)
+  for ( i <- 0 to coeffs.length/2 ) {
+    if(i == coeffs.length/2)
+      sum(i) := delayline(i).i.resized
+    else {
+      sum(i) := delayline(i).i.resize(11 bits) + delayline(coeffs.length-1 - i).i.resize(11 bits)
+      printf("adding delayline(%d) and delayline(%d)\n", i, coeffs.length-1-i)
+    }
+
+    mult_out(i) := (sum(i).resized * S(round(coeffs(i) * 256.0))).resized
+  }
+
+  io.cpx_out.i := mult_out(0) + mult_out(1) + mult_out(2) + mult_out(3)
+
+
+
 }
