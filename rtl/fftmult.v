018c83a7a9c177e087076fa42accd15a5466b989
Michael Nolan <mtnolan2640@gmail.com>
2019-04-23 19:43:00 -0400

    Minor cleanup

--- rtl/fftmult.v
+++ rtl/fftmult.v
@@ -1,25 +1,28 @@
 module fftmult(clk, rst, a, b, o);
    parameter CWIDTH = 18;
    parameter IWIDTH = 18;
+   parameter OWIDTH = (IWIDTH)+CWIDTH;
    parameter CREP = 18-(CWIDTH - 1);
    parameter IREP = 18-(CWIDTH - 1);
    input clk;
    input rst;
    input signed [(CWIDTH-1):0] a;
    input signed [(IWIDTH-1):0] b;
-   output signed [((IWIDTH+1)+CWIDTH-1):0] o;
+   output signed [(OWIDTH-1):0] o;
    initial
      $display("IWIDTH: %d, CWIDTH: %d", IWIDTH, CWIDTH);
 
    wire signed [17:0] 			   a_wire = a;
    wire signed [17:0] 			   b_wire = b;
+   wire signed [35:0] 			   mult_output;
+   assign o = mult_output[OWIDTH-1:0];
 
    // assign o = a_wire*b_wire;
    // always @(a,b,o)
    //   $display("%d * %d = %d", a, b, o);
    mult18x18_1c mult(
    		     // Outputs
-   		     .O			(o),
+   		     .O			(mult_output),
    		     // Inputs
    		     .clk		(clk),
    		     .en		(1'b1),
